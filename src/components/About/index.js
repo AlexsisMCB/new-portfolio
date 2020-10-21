26cec23ec2ac8cd1cf77fbff7ce77754aa126554
Alexsis Conley Brown <alexsismcb@gmail.com>
2020-10-21 05:37:45 -0500

    about me section

---
+++ src/components/About/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import selfImage from '../../assets/images/portfolio.gif';
+
+function About() {
+    return (
+        <section id="who-i-am" className="background-2">
+            <img src={selfImage} className="selfie" style={{ width: "25%" }} alt="selfie" />
+            <p>
+            My full name is Alexsis Conley-Brown.
+            I am currently enrolled in the UW Extended Campus Coding Boot Camp!
+            I take a keen interest in web development and graphic design.
+            Moving forward I plan to continually learn and progress in this line of work.
+            </p>
+        </section>
+    );
+}
+
+export default About;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+
+function Navigation() {
+
+    const titles = [
+        {
+            name: "About Me"
+        },
+        {
+            name: "Portfolio"
+        },
+        {
+            name: "Contact"
+        },
+        {
+            name: "Resume"
+        },
+    ];
+
+    function titleSelected (name) {
+        console.log(`${name} clicked`);
+    }
+
+    return (
+        <header className="d-flex justify-content-between">
+            <div className="symbol-h1">
+                <h1 className="display-4 font-italic"><span> </span>Alexsis Michelle</h1>
+            </div>
+            <nav>
+                <ul className="nav">
+                    {titles.map((title) => (
+                        <ol
+                            className="nav-item"
+                            key={title.name}
+                        >
+                            <span onClick={() => titleSelected(title.name)}>
+                                {title.name}
+                            </span>
+                        </ol>
+                    ))}
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default Navigation;
